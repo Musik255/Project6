21117a26e4f5e0d248324b7978c76f5c71063e8c
P ch <pkchvyrov@vk.com>
2023-11-24 03:28:02 +0300

    completed proj6

--- Project6/Project6/ViewController.swift
+++ Project6/Project6/ViewController.swift
@@ -15,13 +15,13 @@ class TableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        if let startWordsUrl = Bundle.main.url(forResource: "start", withExtension: "txt"){
-//            if let allWordsLongString = try? String(contentsOf: startWordsUrl){
-//                allWords = allWordsLongString.components(separatedBy: "\n")
-//            }
-//        }
+        if let startWordsUrl = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let allWordsLongString = try? String(contentsOf: startWordsUrl){
+                allWords = allWordsLongString.components(separatedBy: "\n")
+            }
+        }
 
-        allWords.append("Телепорт")
+//        allWords.append("Экперимент")
 
         
 //        if allWords.isEmpty{
@@ -30,6 +30,7 @@ class TableViewController: UITableViewController {
         
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addingNewWordAction))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
         
         
         
@@ -37,7 +38,7 @@ class TableViewController: UITableViewController {
         
         
     }
-    func startGame(){
+    @objc func startGame(){
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -57,10 +58,10 @@ class TableViewController: UITableViewController {
     
     
     @objc func addingNewWordAction(){
-        let alertController = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        let alertController = UIAlertController(title: "Напишите слово, состоящее из букв изначального слова", message: nil, preferredStyle: .alert)
         alertController.addTextField()
         
-        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak alertController] action in
+        let submitAction = UIAlertAction(title: "Отправить", style: .default) { [weak self, weak alertController] action in
             
             guard let answer = alertController?.textFields?[0].text else { return }
             self?.submit(answer)
@@ -92,35 +93,35 @@ class TableViewController: UITableViewController {
         if isStartWord(word: lowerAnswer){
             print(lowerAnswer)
             print(title!)
-            alertController.title = "isStartWord"
-            alertController.addAction(UIAlertAction(title: "cancel", style: .cancel))
+            alertController.title = "Это изначальное слово.\nНапишите другое слово."
+            alertController.addAction(UIAlertAction(title: "Закрыть", style: .cancel))
             present(alertController, animated: true)
             return
         }
         if isNotOriginal(word: lowerAnswer){
-            alertController.title = "isOriginal"
-            alertController.addAction(UIAlertAction(title: "cancel", style: .cancel))
+            alertController.title = "Это слово уже в списке.\nНапишите другое слово."
+            alertController.addAction(UIAlertAction(title: "Закрыть", style: .cancel))
             present(alertController, animated: true)
             return
         }
         if isLess3Char(word: lowerAnswer){
-            alertController.title = "isMore3Char"
-            alertController.addAction(UIAlertAction(title: "cancel", style: .cancel))
+            alertController.title = "Нельзя писать слова в которых нет хотя-бы трех букв.\nНапишите другое слово."
+            alertController.addAction(UIAlertAction(title: "Закрыть", style: .cancel))
             present(alertController, animated: true)
             return
         }
         if isNotPossible(word: lowerAnswer){
-            alertController.title = "isPossible"
-            alertController.addAction(UIAlertAction(title: "cancel", style: .cancel))
+            alertController.title = "Некоторые буквы отсутствуют в изначальном слове.\nНапишите другое слово."
+            alertController.addAction(UIAlertAction(title: "Закрыть", style: .cancel))
             present(alertController, animated: true)
             return
         }
-//        if isReal(word: lowerAnswer){
-//            alertController.title = "isReal"
-//            alertController.addAction(UIAlertAction(title: "cancel", style: .cancel))
-//            present(alertController, animated: true)
-//            return
-//        }
+        if isNotReal(word: lowerAnswer){
+            alertController.title = "Такого слова не существует.\nНапишите другое слово."
+            alertController.addAction(UIAlertAction(title: "Закрыть", style: .cancel))
+            present(alertController, animated: true)
+            return
+        }
         
         usedWords.insert(answer, at: 0)
         let indexPath = IndexPath(row: 0, section: 0)
@@ -128,16 +129,16 @@ class TableViewController: UITableViewController {
         
         
     }
+    
     func isStartWord (word : String) -> Bool{
         if word == title!.lowercased(){
-            print("true")
             return true
         }
         else{
-            print("false")
             return false
         }
     }
+    
     func isNotOriginal(word : String) -> Bool{
         return usedWords.contains(word.lowercased())
     }
@@ -165,12 +166,12 @@ class TableViewController: UITableViewController {
     }
     
     
-    func isReal(word : String) -> Bool {
+    func isNotReal(word : String) -> Bool {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false , language: "ru")
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false , language: "en")
         
-        return misspelledRange.location == NSNotFound
+        return misspelledRange.location != NSNotFound
     }
     
     
